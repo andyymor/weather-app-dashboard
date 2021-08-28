6adb3ac680f5d90c715df484656fff936c7f6c2e
andyymor <61852990+andyymor@users.noreply.github.com>
2021-08-27 21:10:34 -0400

    hw

---
+++ scripts.js
@@ -0,0 +1,5 @@
+/* 
+    Use the current weather API (https://openweathermap.org/current) to display the current weather and other info of the city entered by the user in the input after the user presses enter on the input.
+*/
+
+fetch("api.openweathermap.org/data/2.5/weather?q=Miami,fl,&appid={API key}").then(function ))
